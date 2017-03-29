fa25b55a0d87983945213bb516fc337d8976d3ea
nohorjo <nohorjo@gmail.com>
2017-03-29 08:07:55 +0100

    Made narrower

---
+++ FB_Messenger_Narrow_Chat.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Narrow Window
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Makes FB Messenger chat window narrow 
+// @author       Nohorjo
+// @match        https://www.facebook.com/messages/t/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var interval = 1000;
+    function shrink(){
+        try{
+            var width = "550px";
+            var el = document.querySelectorAll("._mh6._wsc")[0].style;
+            if(el.width==width)
+                return;
+            el.width=width;
+        } catch (e){}
+        setTimeout(shrink,interval);
+    }
+    setTimeout(shrink,interval);
+})();
