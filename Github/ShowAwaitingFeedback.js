739247c0c9a93e49c195a7c24d15aad82030d9b6
Muhammed Haque <nohorjo@gmail.com>
2019-07-09 13:41:17 +0100

    Create ShowAwaitingFeedback.js

---
+++ Github/ShowAwaitingFeedback.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Show Awaiting Feedback
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Highlight blocks where the last comment is not you
+// @author       Muhammed Haque
+// @match        https://github.com/livelink/mobile-photos-app/pull/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/nohorjo/Tampermonkey-scripts/master/GitHub/ShowAwaitingFeedback.js
+// @updateURL    https://raw.githubusercontent.com/nohorjo/Tampermonkey-scripts/master/GitHub/ShowAwaitingFeedback.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const user = document.querySelector('img.avatar[src^=http]').attributes.alt.textContent.slice(1);
+
+    document.querySelectorAll('.js-line-comments').forEach(n => {
+        const [ last ] = Array.from(n.querySelectorAll('.review-comment .js-suggested-changes-contents > h4 > strong')).slice(-1);
+
+        if (user === last.textContent.trim()) {
+            n.style.background = "wheat";
+        } else {
+            n.style.background = "lightcoral";
+        }
+    });
+
+
+})();
