80a742231d442021ddc6a6a3bb5255e565382b29
Muhammed Haque <nohorjo@gmail.com>
2019-09-26 09:50:44 +0100

    Only for current user

--- Github/ShowAwaitingFeedback.js
+++ Github/ShowAwaitingFeedback.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show Awaiting Feedback
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Highlight blocks where the last comment is not you
 // @author       Muhammed Haque
 // @match        https://github.com/livelink/mobile-photos-app/pull/*
@@ -14,7 +14,8 @@
     'use strict';
 
     const user = document.querySelector('img.avatar[src^=http]').attributes.alt.textContent.slice(1);
-    if (document.querySelectorAll('.gh-header-meta a.author')[0].textContent === user) {
+
+    if (document.querySelector('a.author').textContent === user) {
         document.querySelectorAll('.js-line-comments').forEach(n => {
             const [ last ] = Array.from(n.querySelectorAll('.review-comment .js-suggested-changes-contents > h4 > strong')).slice(-1);
 
@@ -25,5 +26,4 @@
             }
         });
     }
-
 })();
